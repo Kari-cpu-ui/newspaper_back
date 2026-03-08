79c2762c0e7011d1146c297c4ea87fe8aaa2b616
Karina <mederokarina38@gmail.com>
2026-03-08 21:50:43 +0100

    chore: reorganize project structure and create initial package files

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,34 +1,20 @@
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+import os 
+from sqlalchemy import create_engine # Conexión mysql 
+from sqlalchemy.orm import sessionmaker # Para crear sesiones que interactúan con la DB
 
-
-load_dotenv()
-
-# Construyo la URL de conexion para MySQL usando variables de entorno
-DATABASE_URL = f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}?ssl_mode=REQUIRED"
+# Construyo la URL de conexion para Mysql 
+DATABASE_URL = f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
 
 # Creo el motor de conexión a la base de datos
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,  # Verifica la conexión antes de usarla
+    pool_pre_ping=True, # Verifica la conexión antes de usarla
 )
 
-# Creo SessionLocal, clase para instanciar sesiones con la DB
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+# Creo SessionLocal, una clase que se usará para crear sesiones de base de datos
+SessionsLocal = sessionmaker(
+    autocommit=False,  
+    autoflush=False, # no manda cambios pendientes hasta que lo indicamos
+    bind=engine # vinculala sesisesión con nuestro motor 
+    
 )
-
-# Base declarativa para los modelos
-Base = declarative_base()
-
-# Función para inyectar sesión en FastAPI
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- backend/app/routes/user.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from .. import models, schemas
-from ..database import get_db
-from passlib.context import CryptContext
